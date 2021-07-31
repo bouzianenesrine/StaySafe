687bcb17c0ba00388d23652a682a5cf7f6b70f4e
nesrine <bouziane.nesrine95@gmail.com>
2021-07-31 15:32:45 +0100

    img used

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const perso = document.getElementById("perso");
 const covid = document.getElementById("covid");
+//document.getElementById("HscoreSpan").innerHTML = counter;
+var score = 0;
+var hscore = 0;
+
+
 
 function jump() {
 	if(perso.classList != "jump"){
@@ -19,11 +24,20 @@ let isAlive = setInterval(function() {
 	let covidLeft = parseInt(window.getComputedStyle(covid).getPropertyValue("left"));
 
 	//detect collision
-	if (covidLeft < 70 && covidLeft > 20 && persoTop >= 140) {
+	if (covidLeft < 70 && covidLeft > 20 && persoTop >= 80 /*140*/) {
 		// collision
-		alert("Game over!");
+		alert('Game over! score :'+ score);
+		score = 0;
+	}//calcul score
+	 else {
+		score++;
+		//document.getElementById("hscoreSpan").innerHTML = hscore;
+		document.getElementById("scoreSpan").innerHTML = score;
+		if(score >= hscore){
+			hscore = score
+			document.getElementById("hscoreSpan").innerHTML = hscore;
+		}
 	}
-
 }, 10);
 
 document.addEventListener("keydown", function (event) {
